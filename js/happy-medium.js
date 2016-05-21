8cdb8e465caf88a6e8378714f7f32bbf6ccf3eec
Michael Aubrey <mcaubrey511@gmail.com>
2016-05-21 13:01:46 +0000

    added facebook events

--- js/happy-medium.js
+++ js/happy-medium.js
@@ -2,8 +2,9 @@
 
 function happyMedium(url, callback) {
     url = encodeURIComponent(url);
-    var queryUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + url + "%3Fformat%3Djson%22&callback=?";
 
+    var queryUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + url + "%3Fformat%3Djson%22&callback=?";
+        /* select * from html where url="http://medium.com/akita-association-of-jets?format=json" */
     $.getJSON(queryUrl, function(data) {
         trimQuery(data);
     });
--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 /* global happyMedium */
 
 $(document).ready(function() {
-    happyMedium("medium.com/free-code-camp", function(data){
-    //happyMedium("https://medium.com/akita-association-of-jets", function(data) {
+    //happyMedium("https://medium.com/@michaelcharl.es", function(data) {
+    happyMedium("https://medium.com/akita-association-of-jets", function(data) {
+
         var postData = $.map(data.payload.references.Post, function(el) {
             return el
         });
-        
+        //console.log(postData);
         var buildMediumPosts = function(data, count) {
 
             if (count === undefined) {
@@ -16,13 +17,19 @@ $(document).ready(function() {
 
             var thisPost = data[count];
 
-            if (thisPost && count < 4) {
+            if (thisPost && count < 5) {
                 $(".medium-loading").hide();
                 var $articleCard = $("<a href='https://medium.com/p/" + thisPost.id + "' class='article-card'></div>");
                 var $postImage = $("<div class='article-media'></div>");
                 var $postContent = $("<div class='article-content'></div>");
                 $articleCard.append($postImage);
                 $articleCard.append($postContent);
+                var $articleDate = $("<p class='date'></p>");
+                var pD = new Date(thisPost.firstPublishedAt);
+                var monthNames = ["January", "February", "March", "April", "May", "June",
+                    "July", "August", "September", "October", "November", "December"
+                ];
+                $articleDate.html("Published on " + monthNames[pD.getMonth()] + " " + pD.getDate() + ", " + pD.getFullYear());
                 thisPost.previewContent.bodyModel.paragraphs.forEach(function(preview) {
                     if (preview.type === 1) {
                         var $postDesc = $("<p>" + preview.text + "</p>");
@@ -33,21 +40,23 @@ $(document).ready(function() {
                         $postContent.append($postSubtitle);
                     }
                     else if (preview.type === 4) {
-                        $postImage.css({"background-image": "url(https://cdn-images-1.medium.com/max/500/" + preview.metadata.id });
+                        $postImage.css({
+                            "background-image": "url(https://cdn-images-1.medium.com/max/500/" + preview.metadata.id
+                        });
                     }
                     else if (preview.type === 3) {
                         var $postTitle = $("<h3>" + preview.text + "</h1>");
                         $postContent.append($postTitle);
                     }
                     else if (preview.type === 2) {
-                        var $postPreviewTitle = $("<p>" + preview.text + "</p>");
+                        var $postPreviewTitle = $("<p class='preview-title'>" + preview.text + "</p>");
                         $postContent.append($postPreviewTitle);
                     }
                     else {
                         console.log("Unrecognized preview content type: " + preview.type);
                     }
                 });
-
+                $postContent.append($articleDate);
                 $("#medium-articles").append($articleCard);
 
                 buildMediumPosts(data, count + 1);
@@ -60,5 +69,88 @@ $(document).ready(function() {
 
         buildMediumPosts(postData);
     });
-});
 
+
+
+
+
+    $.ajaxSetup({
+        cache: true
+    });
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function() {
+        FB.init({
+            appId: '1925408434352308',
+            version: 'v2.5', // or v2.0, v2.1, v2.2, v2.3
+        });
+        FB.api(
+            "/923345024404557/events/?access_token=1925408434352308%7C-LqKZs2921LCzVaRg4VMbCHojW4",
+            function(data) {
+                if (data && !data.error) {
+                    /* handle the result */
+                    $(".facebook-loading").hide();
+
+                    function buildEvents(data, count) {
+                        if (!count) {
+                            count = 0;
+                        }
+
+                        var thisEvent = data[count];
+
+                        if (thisEvent && count < 3) {
+
+                            var $eventCard = $('<a href="https://www.facebook.com/events/' + thisEvent.id + '" class="event-card"></a>');
+                            var $eventContent = $('<div class="event-content"></div>');
+                            var $eventTitle = $('<h3></h3>');
+                            var $eventDate = $('<p class="event-date"></p>');
+                            var eD = new Date(thisEvent.start_time);
+                            var monthNames = ["January", "February", "March", "April", "May", "June",
+                                "July", "August", "September", "October", "November", "December"
+                            ];
+                            $eventDate.html(monthNames[eD.getMonth()] + " " + eD.getDate() + ", " + eD.getFullYear());
+                            $eventTitle.html(thisEvent.name);
+                            var $eventDescription = $('<p></p>');
+                            $eventDescription.html(thisEvent.description.substring(0, 100) + "...");
+                            $eventContent.append($eventTitle);
+                            $eventContent.append($eventDate);
+                            $eventContent.append($eventDescription);
+
+                            $eventCard.append($eventContent);
+                            $("#facebook-events").append($eventCard);
+
+                            buildEvents(data, (count + 1));
+                        }
+                        else {
+                            var $moreLink = $("<div class='read-more-card read-more-events'><a href='https://facebook.com/charityakita/events'>See more events on Facebook</a></div>")
+                            $("#facebook-events").append($moreLink);
+                        }
+                    }
+
+                    buildEvents(data.data);
+
+                }
+                else {
+                    console.log(data.error);
+                }
+            }
+        );
+    });
+
+
+
+    function ifURLExists(url, callback) {
+        $.ajax({
+            type: 'HEAD',
+            url: url,
+            success: function() {
+                callback();
+            },
+            error: function(e) {
+                // do nothing
+            }
+        });
+    }
+    
+    ifURLExists("http://archive.charityakita.com", function() {
+        $(".archive-notice").show();
+    });
+});
