bcc9c88e47b8c7c363393918de9b70f01d9345e8
Michael Aubrey <mcaubrey511@gmail.com>
2017-06-11 03:30:02 +0000

    fix Medium API call

--- js/happy-medium.js
+++ js/happy-medium.js
@@ -3,12 +3,17 @@
 function happyMedium(url, callback) {
     "use strict";
     url = encodeURIComponent(url);
-    var queryUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + url + "%3Fformat%3Djson%22&callback=?";
+    var queryUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D%22" + url + "%3Fformat%3Djson%22&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?";
 
     /* select * from html where url="http://medium.com/akita-association-of-jets?format=json" */
     $.getJSON(queryUrl, function (data) {
+        
         var dataString = data.results[0];
-        var jsonString = dataString.substring(18, (dataString.length - 7));
+        //var jsonString = dataString.substring(18, (dataString.length - 7));
+        var jsonString = dataString.substring(70, (dataString.length - 36));
+        //var $displayMe = $('<textarea style="position:fixed;top:0;left:0;height:100vh;width:100vw;z-index:3921231;background:white"></textarea>');
+        //$displayMe.html(jsonString);
+        //$("body").append($displayMe);
         var json = JSON.parse(jsonString);
 
         callback(json);
